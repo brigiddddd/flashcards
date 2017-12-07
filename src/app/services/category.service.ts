c392746cc1efc3aa8e2519ac5a3d6b6a08d8a602
brigid <brigid.halling@gmail.com>
2017-12-07 14:52:24 -0600

    Update category service to use default colors.

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -5,13 +5,19 @@ import { Http, Headers } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
-
 @Injectable()
 export class CategoryService {
   private _categoriesUrl = 'api/categories'; // URL to web api
   private _headers = new Headers({ 'Content-Type:': 'application/json' });
 
-  constructor(private _http: Http) { }
+  defaultBackgroundColor: string;
+  defaultFontColor: string;
+
+  constructor(private _http: Http) {
+    // TODO: MAKE SETTING?
+    this.defaultBackgroundColor = '#fff';
+    this.defaultFontColor = '#000';
+  }
 
   private _handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
@@ -20,14 +26,16 @@ export class CategoryService {
 
   getCategory(id: string): Promise<Category> {
     const url = `${this._categoriesUrl}/${id}`;
-    return this._http.get(url)
+    return this._http
+      .get(url)
       .toPromise()
       .then(response => response.json() as Category)
       .catch(this._handleError);
   }
 
   getCategories(): Promise<Category[]> {
-    return this._http.get(this._categoriesUrl)
+    return this._http
+      .get(this._categoriesUrl)
       .toPromise()
       .then(response => response.json() as Category[])
       .catch(this._handleError);
@@ -39,22 +47,37 @@ export class CategoryService {
       const stack = stacks.find(x => x.id.toString() === stackId);
       stack.categoryId = category.id;
       stack.categoryName = category.name;
-      if (!stack.backgroundColor) { stack.backgroundColor = category.backgroundColor; }
-      if (!stack.fontColor) { stack.fontColor = category.fontColor; }
+      if (!stack.backgroundColor) {
+        stack.backgroundColor = category.backgroundColor;
+      }
+      if (!stack.fontColor) {
+        stack.fontColor = category.fontColor;
+      }
       return stack;
     });
   }
 
   update(category: Category): Promise<Category> {
     const url = `${this._categoriesUrl}/${category.id}`;
-    return this._http.put(url, JSON.stringify(category), { headers: this._headers })
+    return this._http
+      .put(url, JSON.stringify(category), { headers: this._headers })
       .toPromise()
       .then(() => category)
       .catch(this._handleError);
   }
 
   create(name: string): Promise<Category> {
-    return this._http.post(this._categoriesUrl, JSON.stringify({ name: name, cards: [] }), { headers: this._headers })
+    return this._http
+      .post(
+        this._categoriesUrl,
+        JSON.stringify({
+          name: name,
+          cards: [],
+          backgroundColor: this.defaultBackgroundColor,
+          fontColor: this.defaultFontColor
+        }),
+        { headers: this._headers }
+      )
       .toPromise()
       .then(result => result.json() as Category)
       .catch(this._handleError);
@@ -62,7 +85,8 @@ export class CategoryService {
 
   delete(id: number): Promise<void> {
     const url = `${this._categoriesUrl}/${id}`;
-    return this._http.delete(url, { headers: this._headers })
+    return this._http
+      .delete(url, { headers: this._headers })
       .toPromise()
       .then(() => null)
       .catch(this._handleError);
